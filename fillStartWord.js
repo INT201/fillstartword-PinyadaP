d611bd41e074b0e1a818eebd49ef3b8ae42a32df
PinyadaP <pinyada.prae@mail.kmutt.ac.th>
2022-09-04 01:07:52 +0700

    HomeWork1_edit2

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,15 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  function fillStartWord(startWord, word) {
-    //4
-    if(word == null){
-        return underfined
-    }
-    //3
-    else if(word.includes(startWord)==true){
-        return word
-    }
-  }
+  if(word === null || word === undefined){
+    return undefined
+}
+else if(word.includes(startWord)==true){
+    return word
+}
+else{
+    return startWord+word
+}
 }
 module.exports = fillStartWord
